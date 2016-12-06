f7a3ccd5950cdfee49e1ca43870bef8e8fd706cb
Konstantin <kodestruct@gmail.com>
2016-12-06 09:37:26 -0500

    Updated license headers and package references

--- Kodestruct.Concrete/ACI/ACI318_14/C22_SectionalStrength/ShearFriction/ConcreteSectionShearFriction.cs
+++ Kodestruct.Concrete/ACI/ACI318_14/C22_SectionalStrength/ShearFriction/ConcreteSectionShearFriction.cs
@@ -1,5 +1,5 @@
 ﻿#region Copyright
-/*Copyright (C) 2015 Konstantin Udilovich
+   /*Copyright (C) 2015 Konstantin Udilovich
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
    limitations under the License.
    */
 #endregion
-
+ 
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Kodestruct.Steel/AISC/AISC360v10/F_Flexure/Channel/F6-WeakAxis.cs
+++ Kodestruct.Steel/AISC/AISC360v10/F_Flexure/Channel/F6-WeakAxis.cs
@@ -1,4 +1,20 @@
-﻿using Kodestruct.Common.CalculationLogger.Interfaces;
+﻿#region Copyright
+   /*Copyright (C) 2015 Konstantin Udilovich
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */
+#endregion
+using Kodestruct.Common.CalculationLogger.Interfaces;
 using Kodestruct.Common.Section.Interfaces;
 using Kodestruct.Steel.AISC.AISC360v10.General.Compactness;
 using Kodestruct.Steel.AISC.Interfaces;

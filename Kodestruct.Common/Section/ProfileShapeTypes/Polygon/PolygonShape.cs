53488a52d0c45e70618b1e01ec7be1a56eacc930
Konstantin <konstantin.udilovich@wsp.com>
2021-10-31 22:54:53 -0400

    Changed project types

--- Kodestruct.Common/Section/ProfileShapeTypes/Polygon/PolygonShape.cs
+++ Kodestruct.Common/Section/ProfileShapeTypes/Polygon/PolygonShape.cs
@@ -1,30 +1,27 @@
 #region Copyright
-   /*Copyright (C) 2015 Konstantin Udilovich
+/*Copyright (C) 2015 Konstantin Udilovich
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-   */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #endregion
- 
-using System;
-using System.Collections.Generic;
-using MoreLinq;
-using Kodestruct.Common.Mathematics;
-using Kodestruct.Common.Section.Interfaces;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
+
 using ClipperLib;
 using Kodestruct.Common.Mathematics;
+using Kodestruct.Common.Section.Interfaces;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Kodestruct.Common.Section.General
 {
